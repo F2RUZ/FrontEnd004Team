9a2309a42fde75ea290ae10f4c838b3a8c0146df
F2RUZ <gaffarovferuz4@gmail.com>
2025-09-14 18:55:29 +0500

    modefid: file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
+import Business from "./components/Business";
+import Decor from "./components/Decor";
+import Pro from "./components/Pro";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Intro />
+      <Business />
+      <Decor />
+      <Pro />
     </div>
   );
 };
